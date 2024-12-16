4fe044f44dafed90f0daeb8bf0c496cd92ca85fd
fansonly <251115151@qq.com>
2024-12-16 16:27:21 +0800

    feat: utils

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,8 +1,7 @@
 export const isPhone = (value) =>
   /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/.test(value)
 
-export const isEmail = (value) =>
-  /[0-9a-zA-Z_.-]+[@][0-9a-zA-Z_.-]+([.][a-zA-Z]+){1,2}$/.test(value)
+export const isEmail = (value) => /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value)
 
 export const isColor = (color) => {
   const hexColorRegex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/
