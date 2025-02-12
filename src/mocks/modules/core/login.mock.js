b72eb4c86f324bdff30f2e97a6233ea7b3b44b51
fansonly <251115151@qq.com>
2025-02-12 15:12:19 +0800

    style: mock data

--- src/mocks/modules/core/login.mock.js
+++ src/mocks/modules/core/login.mock.js
@@ -16,7 +16,18 @@ export default [
           message: 'success',
           data: {
             accessToken: token,
-            refreshToken: 'refreshToken'
+            refreshToken: 'admin-refresh-token'
+          }
+        }
+      } else if (username == 'guest' && password === md5Encrypt('123456')) {
+        const token =
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InJuZmdvIiwic3ViIjoiYTg0OWRlZmQtY2JiOC00NWIzLTkxNjAtMmE2MWQxOTVjY2I5IiwiaWF0IjoxNzI4MzUwNjg4LCJleHAiOjE3Mjg0MzcwODh9.NBN8mLrLsBw3WcQf3RuxYEaay0z4MkfbrrhuRTecJRM'
+        return {
+          code: 10000,
+          message: 'success',
+          data: {
+            accessToken: token,
+            refreshToken: 'guest-refresh-token'
           }
         }
       } else {
